d61da83d30a140de43755d3d2615be1901fead72
zcd0000 <155466@qq.com>
2016-04-21 16:26:58 +0800

    TEST COMMIT

--- PPTS.Web/app/student/student.service.js
+++ PPTS.Web/app/student/student.service.js
@@ -14,7 +14,8 @@ angular.module('ppts.student.services').factory('studentService', ['$http', func
 	}
 
 	function getDataComplete(response) {
-		return response.data;
+	    return response.data;
+
 	}
 
 	function getDataFailed(error) {
